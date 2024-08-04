bd282b862081c855e6b025547f8f8f9fc6f5b237
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-08-04 23:41:59 +0700

    Вспомнила, как фетчить данные через thunks. Еще раз посмотрела варианты мок апи, остановилась на этом сервисе

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import MenuList from './components/MenuList';
+import { useEffect } from "react";
+import "./App.css";
+import MenuList from "./components/MenuList";
+import { useDispatch, useSelector } from "react-redux";
+import { getSliderAsync } from "./features/sliderSlice";
 
 function App() {
-  return (
-    <div className="App">
-     <MenuList/>
-    </div>
-  );
+  const dispatch = useDispatch();
+
+  const slider = useSelector((state) => state.slider.slider);
+  console.log(slider)
+  useEffect(() => {
+    dispatch(getSliderAsync());
+  }, [dispatch]);
+  return <div className="App">
+    {slider?(slider.map((item)=>{
+      return (
+        <img src={item} alt=''/>
+      )
+    })):''}
+  </div>;
 }
 
 export default App;
---
+++ src/features/sliderSlice.jsx
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+
+export const getSliderAsync = createAsyncThunk(
+	'slider/getSliderAsync',
+	async () => {
+		const resp = await fetch('https://668160c404acc3545a068660.mockapi.io/api/store/data');
+		if (resp.ok) {
+			const data = await resp.json();
+            const slider = data[0]
+			return { slider };
+		}
+	}
+);
+
+
+
+export const sliderSlice = createSlice({
+	name: 'slider',
+	initialState: [],
+	reducers: {
+		
+	},
+    extraReducers: (builder) => {
+        // Add reducers for additional action types here, and handle loading state as needed
+        builder.addCase(getSliderAsync.fulfilled, (state, action) => {
+          // Add user to the state array
+          return action.payload.slider;
+        })
+      },
+	// extraReducers: {
+	// 	[getSliderAsync.fulfilled]: (state, action) => {
+	// 		return action.payload.slider;
+	// 	},
+		
+		
+	// },
+});
+
+// export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions;
+
+export default sliderSlice.reducer;
