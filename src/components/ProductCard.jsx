cd852fc93654c8832580b1c58f529eecd2ba7a47
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-08-08 23:42:33 +0700

    Снова повторю, что сервис mockapi имеет ограничения. Но в целом он очень понятен. Ограничения снять не могу, потому что он зарубежный. Суть в чем: я вынуждена немного танцевать с бубном. То, что должно фильтроваться на стороне сервере будет фильтроваться на стороне клиента. В этом коммите в mockapi добавлено свойство category, которое нужно исключительно для корректного роутинга.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const ProductCard = ({product}) => {
+const ProductCard = ({product, category}) => {
   return (
     <div className='productCard'>
+    <Link to={`/categories/${product.category}/${product.id}`}>
       <img src={product.img} alt={product.title}/>
+      <div>
       <h3>{product.title}</h3>
       <p>{product.description}</p>
       <p>{product.price}</p>
       <p>{product.weight}</p>
+      </div>
+      </Link>
       <p>{product.amountInCart>0?'В корзине':'Добавить в корзину'}</p>
     </div>
   )
