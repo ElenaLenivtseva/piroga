fda16583a4561d8a221377b9d95c57d451b8cc5d
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-08-10 23:53:13 +0700

    А вот и танцы с бубном, о чем я писала до этого: из-за ограниений сервиса mockpi по количеству uri, куда можно постучаться с запросами, фильтрация проходит на стороне клиента. Реализована фильтрация в компоненте singleroduct. Поиск нужного продукта идет не через фильтрацию в одном из экшенов (не через redux в общем), а таким образом: при первичном рендеринге идет запрос на все продукты сразу. Затем, через useparams получается из адресной строки объект с двумя параметрами: type (отвечает за категорию, где нужно искать продукт), id (отвечает за поиск конкретного продукта в категории). Возвращает почему-то всегда массив. Фильтрация в два этапа: категория, конкретный продукт.

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategoriesAsync } from "../features/productsSlice";
 
-const SingleProduct = ({ id }) => {
+const SingleProduct = () => {
+  const params = useParams();
+  console.log(params.id)
+  const dispatch = useDispatch();
+
+  const products = useSelector((state) => state.products);
+
+  let allCategory = products.filter((elem) => {
+    return elem.type === params.type;
+  });
+
+  let singleProduct = allCategory[0].products.filter((elem)=>{
+    return elem.id === params.id
+  })
+  
+  useEffect(() => {
+    dispatch(getCategoriesAsync());
+  }, [dispatch]);
+
   // получить все-все товары соответсвующей категории, затем по id конкретного продукта, получить все данные о нем - в объекте и отрисовать по нему
   return <div>SingleProduct</div>;
 };
